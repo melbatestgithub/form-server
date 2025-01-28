79ff491b9b90717104d959e7b74a924307616bb2
melaku <melakuzeleke443@gmail.com>
2025-01-28 04:17:32 +0300

    some change

--- routes/VideoFeedback.js
+++ routes/VideoFeedback.js
@@ -1,3 +1,48 @@
 const express=require('express')
 const router=express.Router()
+const Video=require('../models/VideoFeedback')
+
+router.post("/submit-form", async (req, res) => {
+    const {
+      satisfaction,
+      ambiance,
+      selectedType,
+      video,
+      videoFeedback,
+      phone,
+      email,
+    } = req.body;
+  
+    try {
+      // Create a new entry in the Videos table
+      const newVideo = await Video.create({
+        satisfaction,
+        ambiance,
+        selectedType,
+        video,
+        videoFeedback,
+        phone,
+        email,
+      });
+  
+      res.status(201).json({
+        message: "Form submitted successfully!",
+        data: newVideo,
+      });
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      res.status(500).json({
+        message: "Error submitting form",
+        error: error.message,
+      });
+    }
+  });
+  router.get("/", async (req, res) => {
+    try {
+      const feedbackData = await Video.findAll();
+      res.status(200).send({ data: feedbackData });
+    } catch (error) {
+      res.status(500).send({ message: "Error fetching data", error: error.message });
+    }
+  });
 module.exports=router
